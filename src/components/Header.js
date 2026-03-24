3311cded3a5778ae211ed2e2e460e38659502fc7
unknown <samuel.vianna010@gmail.com>
2026-03-24 17:34:07 -0300

    pagina de cadastro pronta

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
+import "../assets/fontawesome/css/fontawesome.css";
+import "../assets/fontawesome/css/solid.css";
+
 export default function Header() {
   return (
     <>
       <header className="bg-teal-800 flex flex-row items-center px-10 py-5 gap-5">
         <h3 className="text-white text-2xl font-bold">Nome da Empresa</h3>
-        <nav className="flex flex-1 flex-row justify-between">
+        <nav className="flex flex-1 flex-row justify-between px-40">
           <a href="" className="text-white text-xl font-extrabold">
             Home
           </a>
@@ -14,11 +17,11 @@ export default function Header() {
             Comprar Créditos
           </a>
         </nav>
-        <div className="flex flex-row gap-4 items-center">
-          <h3 className="text-white text-xl">Usuário</h3>
-          <svg className="w-[60px] h-[60px] flex items-center justify-center">
-            <circle cx="30" cy="30" r="30" fill="white" />
-          </svg>
+        <div className="flex flex-row gap-4 items-center pl-10">
+          <h3 className="text-white text-xl font-semibold">Usuário</h3>
+          <div className="w-[60px] h-[60px] bg-teal-900 rounded-full flex items-center justify-center">
+            <i className="fa-solid fa-user text-teal-100 text-2xl"></i>
+          </div>
         </div>
       </header>
     </>
